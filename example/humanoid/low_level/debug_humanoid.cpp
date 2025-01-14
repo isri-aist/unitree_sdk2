9ea0df31fecb610a8e1ddfc1ebbdd586a97c4a31
Pierre-Alexandre Leziart <pa.leziart@aist.go.jp>
2025-01-14 18:02:53 +0900

    Edit debug main to call to wrapper

--- example/humanoid/low_level/debug_humanoid.cpp
+++ example/humanoid/low_level/debug_humanoid.cpp
@@ -4,13 +4,48 @@
 #include <stdint.h>
 #include <string>
 #include <thread>
+#include <chrono>
 
-#include "cpuMLP/Interface.hpp"
 #include "cpuMLP/Types.h"
-
+#include "OnnxWrapper.hpp"
 #include "Joystick.hpp"
 
-int main() {
+int main(int argc, ORTCHAR_T* argv[]) {
+
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::duration;
+  using std::chrono::microseconds;
+
+  std::cout << "Hello there!" << std::endl;
+  
+  if (argc != 2) {
+    std::cout << "Usage: ./onnx-api-example <onnx_model.onnx>" << std::endl;
+    return -1;
+  }
+
+  std::basic_string<ORTCHAR_T> model_file = argv[1];
+  
+  OnnxWrapper wrapper(model_file);
+  wrapper.initialize();
+
+  // Run one inference
+  VectorM A = wrapper.run(Eigen::VectorXf::Zero(28 * 28));
+  std::cout << "Output:" << std::endl << A.transpose() << std::endl;
+
+  // Time the average computation time for inference
+  auto t1 = high_resolution_clock::now();
+  for(int i = 0; i < 100; i++) {
+    wrapper.run(Eigen::VectorXf::Zero(28 * 28));
+  }
+  auto t2 = high_resolution_clock::now();
+
+  // Getting number of microseconds as a double.
+  duration<double, std::micro> ms_double = t2 - t1;
+
+  std::cout << "Average computation time: " << ms_double.count() / 100 << " us\n";
+
+  return 0;
 
   using namespace std::chrono_literals;
 
@@ -20,23 +55,45 @@ int main() {
       0.0};                                                  // Unused joint
 
   // MLP interface
-  Interface mlpInterface_(55, 3, 14, 1, 1);
+  Interface mlpInterface_(51, 0, 10, 1, 1);
+
+  Vector10 q_init_cut = q_init.head(10);
+  // Vector4 q_arms; q_arms << 0.4, -0.4, 0.4, -0.4;
+  // q_init_cut.block(10, 0, 4, 1) << 0.4, -0.4, 0.4, -0.4;
 
   // Create link with MLP
   mlpInterface_.initialize(
-      "/home/paleziart/git/policies/H1Terrain_09-25_18-22-36/nn/",
-      q_init.head(10), 0.5, 0.02);
+      "/home/paleziart/git/policies/H1Terrain_10-01_18-04-38/nn/",
+      q_init_cut, 0.5, 0.02);
 
-  Vector14 pos = q_init.head(14);
-  Vector14 vel = Vector14::Zero();
+  Vector10 pos = q_init.head(10);
+  pos << 0.0, 0.0, -0.2, 0.6, -0.4, 0.0, 0.0, -0.2, 0.6,  -0.4; // , 0.4, 0.0, 0.4, 0.0;
+  Vector10 vel = Vector10::Zero();
+  Vector10 tau = Vector10::Zero();
   Vector4 ori{0.0, 0.0, 0.0, 1.0};
+  Vector3 rpy = Vector3::Zero();
   Vector3 gyro = Vector3::Zero();
   Vector6 cmd = Vector6::Zero();
   float time = 0.0;
-  mlpInterface_.update_observation(pos.head(14), vel.head(14), ori, gyro, cmd,
-                                   time);
 
-  mlpInterface_.forward();
+  std::cout << "Q" << std::endl << pos.transpose() << std::endl;
+  std::cout << "dQ" << std::endl << vel.transpose() << std::endl;
+  std::cout << "TAU" << std::endl << tau.transpose() << std::endl;
+  std::cout << "ORI" << std::endl << ori.transpose() << std::endl;
+  std::cout << "GYRO" << std::endl << gyro.transpose() << std::endl;
+  std::cout << "CMD" << std::endl << cmd.transpose() << std::endl; 
+
+  for (int i = 0; i < 2; i++) {
+
+    std::cout << "---- " << i << std::endl;
+
+    mlpInterface_.update_observation(pos.head(10), vel.head(10), tau.head(10), rpy, gyro,
+                                     cmd, time);
+
+    mlpInterface_.forward();
+
+  }
+  return 0;
 
   Joystick joy;
   joy.initialize(0.02);
