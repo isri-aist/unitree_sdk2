49ec39f995efc058069bccb82ffb435e973cebc3
Pierre-Alexandre Leziart <pa.leziart@aist.go.jp>
2025-01-28 11:24:22 +0900

    Removed unused test in debug_humanoid

--- example/humanoid/low_level/debug_humanoid.cpp
+++ example/humanoid/low_level/debug_humanoid.cpp
@@ -60,53 +60,9 @@ int main(int argc, ORTCHAR_T *argv[]) {
 
   return 0;
 
+  // TESTING LINK WITH THE JOYSTICK
+
   using namespace std::chrono_literals;
-
-  const Vector20 q_init{
-      0.0, 0.0, -0.2, 0.6, -0.4, 0.0, 0.0, -0.2, 0.6,  -0.4, // Legs
-      0.0, 0.4, 0.0,  0.0, -0.4, 0.4, 0.0, 0.0,  -0.4,       // Torso and arms
-      0.0};                                                  // Unused joint
-
-  // MLP interface
-  Interface mlpInterface_;
-
-  Vector10 q_init_cut = q_init.head(10);
-  // Vector4 q_arms; q_arms << 0.4, -0.4, 0.4, -0.4;
-  // q_init_cut.block(10, 0, 4, 1) << 0.4, -0.4, 0.4, -0.4;
-
-  // Create link with MLP
-  mlpInterface_.initialize(
-      "/home/paleziart/git/policies/H1Terrain_10-01_18-04-38/nn/", q_init_cut,
-      0.02);
-
-  Vector10 pos = q_init.head(10);
-  pos << 0.0, 0.0, -0.2, 0.6, -0.4, 0.0, 0.0, -0.2, 0.6,
-      -0.4; // , 0.4, 0.0, 0.4, 0.0;
-  Vector10 vel = Vector10::Zero();
-  Vector10 tau = Vector10::Zero();
-  Vector4 ori{0.0, 0.0, 0.0, 1.0};
-  Vector3 rpy = Vector3::Zero();
-  Vector3 gyro = Vector3::Zero();
-  Vector6 cmd = Vector6::Zero();
-  float time = 0.0;
-
-  std::cout << "Q" << std::endl << pos.transpose() << std::endl;
-  std::cout << "dQ" << std::endl << vel.transpose() << std::endl;
-  std::cout << "TAU" << std::endl << tau.transpose() << std::endl;
-  std::cout << "ORI" << std::endl << ori.transpose() << std::endl;
-  std::cout << "GYRO" << std::endl << gyro.transpose() << std::endl;
-  std::cout << "CMD" << std::endl << cmd.transpose() << std::endl;
-
-  for (int i = 0; i < 2; i++) {
-
-    std::cout << "---- " << i << std::endl;
-
-    mlpInterface_.update_observation(pos.head(10), vel.head(10), tau.head(10),
-                                     rpy, ori, gyro, cmd, time);
-
-    mlpInterface_.forward();
-  }
-  return 0;
 
   Joystick joy;
   joy.initialize(0.02);
