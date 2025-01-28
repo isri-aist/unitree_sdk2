2d51b11b3a26d72a62f2b774e62bd9889c549251
Pierre-Alexandre Leziart <pa.leziart@aist.go.jp>
2025-01-28 11:41:44 +0900

    Reduce init duration to 5s

--- example/humanoid/low_level/humanoid.hpp
+++ example/humanoid/low_level/humanoid.hpp
@@ -492,7 +492,7 @@ private:
 
   float time_ = 0.f;
   float time_run_ = 0.f;
-  const float init_duration_ = 8.f;
+  const float init_duration_ = 5.f;
   const float interp_duration_ = 0.1f;
 
   float report_dt_ = 0.1f;
