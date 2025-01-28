502caf46f73c299a718175eac8cbbb84376a8d0b
Pierre-Alexandre Leziart <pa.leziart@aist.go.jp>
2025-01-28 11:03:55 +0900

    Clean Types.h

--- example/humanoid/low_level/Types.h
+++ example/humanoid/low_level/Types.h
@@ -9,6 +9,13 @@
 #include <memory>
 #include <string>
 
+// DYNAMIC SIZED TYPES
+using Vxf = Eigen::VectorXf;
+using Vxi = Eigen::VectorXi;
+using Mxf = Eigen::MatrixXf;
+using Matrix6N = Eigen::Matrix<float, 6, Eigen::Dynamic>;
+
+// FIXED SIZED TYPES
 using Vector1 = Eigen::Matrix<float, 1, 1>;
 using Vector2 = Eigen::Matrix<float, 2, 1>;
 using Vector3 = Eigen::Matrix<float, 3, 1>;
@@ -17,36 +24,10 @@ using Vector5 = Eigen::Matrix<float, 5, 1>;
 using Vector6 = Eigen::Matrix<float, 6, 1>;
 using Vector7 = Eigen::Matrix<float, 7, 1>;
 using Vector8 = Eigen::Matrix<float, 8, 1>;
+using Vector9 = Eigen::Matrix<float, 9, 1>;
 using Vector10 = Eigen::Matrix<float, 10, 1>;
-using Vector11 = Eigen::Matrix<float, 11, 1>;
-using Vector12 = Eigen::Matrix<float, 12, 1>;
-using Vector14 = Eigen::Matrix<float, 14, 1>;
-using Vector18 = Eigen::Matrix<float, 18, 1>;
 using Vector19 = Eigen::Matrix<float, 19, 1>;
 using Vector20 = Eigen::Matrix<float, 20, 1>;
-using Vector24 = Eigen::Matrix<float, 24, 1>;
-using Vector123 = Eigen::Matrix<float, 123, 1>;
-using Vector132 = Eigen::Matrix<float, 132, 1>;
-using VectorN = Eigen::Matrix<float, Eigen::Dynamic, 1>;
-using VectorNi = Eigen::Matrix<int, Eigen::Dynamic, 1>;
-
-using Matrix2 = Eigen::Matrix<float, 2, 2>;
-using Matrix3 = Eigen::Matrix<float, 3, 3>;
-using Matrix4 = Eigen::Matrix<float, 4, 4>;
-using Matrix12 = Eigen::Matrix<float, 12, 12>;
-
-using Matrix13 = Eigen::Matrix<float, 1, 3>;
-using Matrix14 = Eigen::Matrix<float, 1, 4>;
-using Matrix112 = Eigen::Matrix<float, 1, 12>;
-using Matrix118 = Eigen::Matrix<float, 1, 18>;
-using Matrix34 = Eigen::Matrix<float, 3, 4>;
-using Matrix43 = Eigen::Matrix<float, 4, 3>;
-using Matrix64 = Eigen::Matrix<float, 6, 4>;
-using Matrix74 = Eigen::Matrix<float, 7, 4>;
-using MatrixN4 = Eigen::Matrix<float, Eigen::Dynamic, 4>;
-using Matrix3N = Eigen::Matrix<float, 3, Eigen::Dynamic>;
-using Matrix6N = Eigen::Matrix<float, 6, Eigen::Dynamic>;
-using MatrixN = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>;
 
 using VectorM = Vector10;
 
