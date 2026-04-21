2ce78e50f86e5a6466c9315d616b1a5be4c6e3ca
Pierre-Alexandre Leziart <pa.leziart@aist.go.jp>
2026-04-21 17:40:11 +0900

    Use Joystick and deploy on real robot

--- example/h1/low_level/humanoid.hpp
+++ example/h1/low_level/humanoid.hpp
@@ -27,7 +27,7 @@
 
 #include <unitree/robot/b2/motion_switcher/motion_switcher_client.hpp>
 
-#define USE_JOYSTICK false
+#define USE_JOYSTICK true
 
 #define STATUS_INIT 0
 #define STATUS_WAITING_AIR 1
@@ -48,8 +48,8 @@ public:
                   const std::string &kpScaling = "0.0")
       : networkInterface_() {
 
-    unitree::robot::ChannelFactory::Instance()->Init(1, "lo");
-    // unitree::robot::ChannelFactory::Instance()->Init(0, networkInterface);
+    // unitree::robot::ChannelFactory::Instance()->Init(1, "lo");
+    unitree::robot::ChannelFactory::Instance()->Init(0, networkInterface);
     std::cout << "Initialize channel factory." << std::endl;
 
     msc.reset(new unitree::robot::b2::MotionSwitcherClient());
